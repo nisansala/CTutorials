3f8d09b87ea7b9c6942003011bcaa4f266e70c40
nisansala <nisansala.nx@gmail.com>
2018-06-17 19:02:53 +0530

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+	printf("University of Moratuwa\n");
+	printf("Katubedda,\n");
+	printf("Moratuwa\n");
+	printf("Sri Lanka\n");
+	printf("------------------\n");
+	printf("www.mrt.ac.lk\n");
+
+
+}
